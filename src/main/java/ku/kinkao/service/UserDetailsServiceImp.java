43bab896897b18463ce9a6ce8af4441291264a67
teenteennnii <teennopwijit@gmail.com>
2024-05-07 19:56:05 +0700

    add logging

--- src/main/java/ku/kinkao/service/UserDetailsServiceImp.java
+++ src/main/java/ku/kinkao/service/UserDetailsServiceImp.java
@@ -2,12 +2,16 @@ package ku.kinkao.service;
 
 import ku.kinkao.entity.Member;
 import ku.kinkao.repository.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +19,7 @@ import java.util.List;
 @Service
 public class UserDetailsServiceImp implements UserDetailsService {
 
+    Logger logger = LoggerFactory.getLogger(UserDetailsServiceImp.class);
 
    @Autowired
    private MemberRepository memberRepository;
@@ -34,6 +39,9 @@ public class UserDetailsServiceImp implements UserDetailsService {
 
        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
        authorities.add(new SimpleGrantedAuthority(member.getRole()));
+
+       logger.info(username + " has successfully logged in at "
+               + Instant.now());
 
        return new org.springframework.security.core.userdetails.User(
                member.getUsername(), member.getPassword(),
